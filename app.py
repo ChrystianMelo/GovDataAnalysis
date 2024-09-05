37e55e0a6b5c910911e7752ba7e67ae9273a6438
Chrystian Melo <meloo.chrys@gmail.com>
2024-09-05 17:04:12 -0300

    Removed #18e2a50

--- app.py
+++ app.py
@@ -279,11 +279,10 @@ if not (os.path.exists(file_path)):
     graduacao =  pd.read_csv(graduacao_path)
 
     especializacao_path = "especializacao.csv"
-    os.remove(especializacao_path)
-    #if not (os.path.exists(especializacao_path)):
+    if not (os.path.exists(especializacao_path)):
         # O site do mec demora muito para carregar, então vou baixar a partir do meu driver pessoal.
         #downloadFile(especializacao_path, "https://olinda.mec.gov.br/olinda-ide/servico/PDA_SERES/versao/v1/odata/PDA_Cursos_Especializacao_Brasil?$format=text/csv")
-    downloadFile("especializacao.csv", "https://drive.google.com/uc?id=15Mgq9U3C6775p5AoLdKBmP1-AmiC24_0")
+        downloadFile("especializacao.csv", "https://drive.google.com/uc?id=15Mgq9U3C6775p5AoLdKBmP1-AmiC24_0")
     especializacao = pd.read_csv(especializacao_path)
     
     conn = sqlite3.connect(file_path)
